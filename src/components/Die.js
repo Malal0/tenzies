0f9978bb5977e79d0f02853816ff8308eb42396e
UncomfortableChai <mj1055119@gmail.com>
2022-05-29 02:07:02 -0700

    Added roll btn functionality

---
+++ src/components/Die.js
@@ -0,0 +1,6 @@
+export default function Die(props) {
+    return (
+        <div className="die">{props.value}</div>
+        /*<div className="die active">1</div>*/
+    )
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,33 @@
+import React from "react"
+import Die from "./Die"
+
 export default function Main() {
+
+    const [dice, setDice] = React.useState(allNewDice)
+
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+
+    function rollDice() {
+        setDice(allNewDice())
+    }
+
+    const diceElements = dice.map(die => <Die value={die} />)
+
     return (
         <section>
             <div className="main">
                 <h1 className="title">tenzies</h1>
                 <p className="instructions">Roll until all dice are the same. Click each die to freeze it at it's current value between rolls.</p>
                 <div className="dieContainer">
-                    <div className="die active">1</div>
-                    <div className="die">2</div>
-                    <div className="die active">1</div>
-                    <div className="die">4</div>
-                    <div className="die">5</div>
-                    <div className="die">3</div>
-                    <div className="die">3</div>
-                    <div className="die">5</div>
-                    <div className="die">6</div>
-                    <div className="die active">1</div>
+                    {diceElements}
                 </div>
-                <button className="rollBtn">roll</button>
+                <button className="rollBtn" onClick={rollDice}>roll</button>
             </div>
         </section>
     )
