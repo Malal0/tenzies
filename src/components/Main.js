cd79fe96ff28332a817be7d79926a41034ad4479
UncomfortableChai <mj1055119@gmail.com>
2022-06-04 13:15:51 -0700

    Added a '0' in front of the numbers on the timer

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,9 +14,9 @@ export default function Main() {
     const [count, setCount] = React.useState(0)
 
     const [gameTime, setGameTime] = React.useState({
-        minutes: 0,
+        milliseconds: 0,
         seconds: 0,
-        milliseconds: 0
+        minutes: 0
     })
 
     React.useEffect(() => {
@@ -26,6 +26,9 @@ export default function Main() {
         if (allHeld && allSameValue) {
             setTenzies(true)
             console.log("You won")
+
+            stopTimer()
+            console.log("stopped")
         }
     }, [dice])
 
@@ -84,35 +87,64 @@ export default function Main() {
     let ms = 0
     let sec = 0
     let min = 0
-    setInterval(() => {
-        ms += 250
-        if (ms >= 1000) {
+
+    function recordTime() {
+        setInterval(() => {
+            ms += 250
+            setGameTime(prevTime => ({
+                ...prevTime,
+                milliseconds: ms
+            }))
+
+            if (ms >= 1000) {
+                ms = 0
+                sec++
+                setGameTime(prevTime => ({
+                    ...prevTime,
+                    seconds: sec,
+                }))
+            }
+            if (sec >= 60) {
+                sec = 0
+                min++
+                setGameTime(prevTime => ({
+                    ...prevTime,
+                    minutes: min,
+                }))
+            }
+        }, 250)
+    }
+
+    React.useEffect(() => {
+        const someHeld = dice.some(die => die.isHeld)
+        const allHeld = dice.every(die => die.isHeld)
+
+        if (tenzies === false && count === 0 && !someHeld) {
             ms = 0
-            sec++
+            setGameTime(prevTime => ({
+                minutes: 0,
+                seconds: 0,
+                milliseconds: 0
+            }))
+            console.log("reseted")
+        } else if (someHeld || count > 0) {
+            recordTime()
         }
+    }, [dice])
 
-        if (sec >= 60) {
-            sec = 0
-            min++
-        }
-
-        console.log(`Timer: ${min.toFixed(2)}m :${sec.toFixed(2)}s :${ms.toFixed()}ms`)
-
-    }, 250)
-
-    // function timer() {
-    //     let ms = 0
-
-
-    //     console.log(ms)
-    // }
-    // timer()
+    function stopTimer() {
+        clearInterval(recordTime())
+    }
 
     return (
         <section>
             <div className="main">
                 {tenzies && <Confetti />}
-                <Timer min={min} sec={sec} millisec={ms} />
+                <Timer
+                    min={gameTime.minutes >= 10 ? gameTime.minutes : "0" + gameTime.minutes}
+                    sec={gameTime.seconds >= 10 ? gameTime.seconds : "0" + gameTime.seconds}
+                    millisec={gameTime.milliseconds >= 10 ? gameTime.milliseconds : "0" + gameTime.milliseconds}
+                />
                 <Count count={count} />
                 <h1 className="title">tenzies</h1>
                 <p className="instructions">Roll until all dice are the same. Click each die to freeze it at it's current value between rolls.</p>
